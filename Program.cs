19aab85cb986721dadfd86889bbef7a4e0ec7694
Yuriiru <98612792+Yuriiru@users.noreply.github.com>
2023-03-23 11:05:19 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,85 +1,89 @@
-﻿using System;
-using Student_Namespace;
 using Reader_Namespace;
+using Student_Namespace;
 
-namespace Student_Namespace{
-    class Student
+namespace Student_Namespace
+{
+    public class Student
     {
-        public string FIO;
-        public string Group_Number;
-        private int Age;
 
-        public Student(string fIO, string group_Number, int age)
+        public string Fio;
+        public string group;
+        private int age;
+
+        public Student(string Fio, string group, int age)
         {
-            this.FIO = fIO;
-            this.Group_Number = group_Number;
-            this.Age = age;
+            this.Fio = Fio;
+            this.group = group;
+            this.age = age;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("фамилия" + Fio + "группа" + group + "возраст" + age);
         }
     }
-
 }
-
 namespace Reader_Namespace
 {
-    class Reader
+    public class Reader
     {
-        private string FIO;
-        public int library_card_number;
+        private string fio;
+        public int numberReadTick;
         public string faculty;
-        private int Age;
-        public long Phone_Number;
+        private string bd;
+        public string telephone;
 
-        public Reader(string fIO, int library_card_number, string faculty, int age, long phone_Number)
+        public Reader(string fio, int numberReadTick, string faculty, string bd, string telephone)
         {
-            this.FIO = fIO;
-            this.library_card_number = library_card_number;
+            this.fio = fio;
+            this.numberReadTick = numberReadTick;
             this.faculty = faculty;
-            this.Age = age;
-            this.Phone_Number = phone_Number;
+            this.bd = bd;
+            this.telephone = telephone;
         }
 
-        public void takeBook(int number_of_books)
+        public void takeBook(int numberBook)
         {
-            Console.WriteLine(String.Format("{0} взял {1} книг(и)"), FIO, number_of_books);
+            Console.WriteLine(String.Format("{0} взял {1} книг(и)"), fio, numberBook);
         }
 
-        public void takeBook(string[] names_books)
+        public void takeBook(string namesBook)
         {
-            Console.WriteLine(String.Format("{0} взял {1} книг(и)"), FIO, names_books);
+            Console.WriteLine(String.Format("{0} взял {1} книг(и)"), fio, namesBook);
         }
 
-        public void returnBook(int number_of_books)
+        public void returnBook(int numberBook)
         {
-            Console.WriteLine(String.Format("{0} ввернул {1} книг(и)"), FIO, number_of_books);
-        }
-        public void returnBook(string[] names_books)
-        {
-            Console.WriteLine(String.Format("{0} вернул {1} книг(и)"), FIO, names_books);
+            Console.WriteLine(String.Format("{0} взял {1} книг(и)"), fio, numberBook);
         }
 
+        public void returnBook(string namesBook)
+        {
+            Console.WriteLine(String.Format("{0} взял {1} книг(и)"), fio, namesBook);
+        }
     }
 }
-namespace Laba5_W
+namespace laba_5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
             Student[] students =
             {
-                new Student("Васичкин А. И.", "21РТ31", 18),
-                new Student("Галич З. В.", "21РТ31", 18),
-                new Student("Зиманов И. Г.", "21РТ31", 18) 
+                new Student("Русинов Ю. А.","20ИТ17", 19),
+                new Student("Жирнова Е. А.", "20ИТ17", 18),
+                new Student("Марусик М. А.","20ИТ17", 19)
             };
 
-            Reader[] library_users =
+            Reader[] libraryUsers =
             {
-                new Reader("Васичкин А. И.", 1001230075, "Науки и техники", 18, 89278523561),
-                new Reader("Галич З. В.", 1001220075, "Науки и техники", 18, 89278534546),
-                new Reader("Зиманов И. Г.", 1001210075, "Науки и техники" , 18, 89278435561)
+                new Reader("Русинов Ю. А.",8395883 , "Программирование", "22.02.04","89095638858"),
+                new Reader("Жирнова Е. А.",63754845 , "Моделирование", "10.06.04","8994738836"),
+                new Reader("Марусик М. А.", 23756347, "Экология", "17.01.04","89635683400")
             };
 
+          
         }
     }
 }
